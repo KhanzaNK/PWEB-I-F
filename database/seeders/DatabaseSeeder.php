640f36f6a18631ac0176d32cdf4da1fa815801f9
Khanza Nibras Kadir <khanzakadir@gmail.com>
2026-01-05 13:17:03 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\SampahSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -22,6 +21,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(SampahSeeder::class);
+        $this->call(::class);
     }
 }
