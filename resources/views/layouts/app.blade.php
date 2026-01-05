57cb6b3072851c5e60149d3a243f512306bc9bcd
KhanzaNK <khanzakadir@gmail.com>
2026-01-05 11:10:51 +0700

    Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,8 @@
     <meta charset="UTF-8">
     <title>@yield('title', 'EcoWaste')</title>
 
-    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Chart.js -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
     <style>
@@ -33,12 +31,11 @@
             border-bottom: 2px solid #22c55e;
             font-weight: 600;
         }
-        /* TRANSISI DASAR */
+
         .glow-soft {
             transition: all 0.3s ease;
         }
 
-        /* ORGANIK */
         .glow-organik:hover {
             box-shadow: 
                 0 0 0 2px #22c55e,
@@ -46,7 +43,6 @@
             transform: translateY(-8px);
         }
 
-        /* ANORGANIK */
         .glow-anorganik:hover {
             box-shadow: 
                 0 0 0 2px #3b82f6,
@@ -54,22 +50,19 @@
             transform: translateY(-8px);
         }
 
-        /* B3 */
         .glow-b3:hover {
             box-shadow: 
                 0 0 0 2px #ef4444,
                 0 10px 30px rgba(239, 68, 68, 0.45);
             transform: translateY(-8px);
         }
-        /* WRAPPER AGAR PERSIS TENGAH */
         .kategori-wrapper {
-            max-width: 1100px; /* ini kunci biar mirip Figma */
+            max-width: 1100px; 
         }
 
-        /* CARD KATEGORI */
         .kategori-card {
             width: 100%;
-            max-width: 320px; /* ukuran kartu ala Figma */
+            max-width: 320px; 
             padding: 24px;
             text-align: center;
         }
@@ -78,7 +71,6 @@
     </style>
 
     <style>
-    /* FOOTER DARK STYLE */
         .footer-dark {
             background: linear-gradient(180deg, #0f172a, #020617);
             color: #e5e7eb;
@@ -118,7 +110,6 @@
             color: #cbd5f5;
         }
 
-        /* SOCIAL ICON */
         .social-icon {
             width: 40px;
             height: 40px;
