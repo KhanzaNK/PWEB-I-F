ae25514f4c811c1d44d23071daa91dadd5c8eb6d
amriR01 <amrirasyid014@gamil.com>
2026-01-05 13:03:24 +0700

    menyelesaikan halaman jual baru

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,3 @@ Route::get('/jual', function () {
     return view('jual');
 });
 
-Route::get('/home', function () {
-    return "ini halaman home";
-});
-
-Route::get('/toko', function () {
-    return "ini halaman toko";
-});
-
-Route::get('/jual_sampah', function () {
-    return "ini halaman jual sampah";
-});
-
-Route::get('/login', function () {
-    return "ini halaman login";
-});
